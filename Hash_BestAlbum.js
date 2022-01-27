52323dff6f70e36ae1654f9678a902ed2992f3e4
Ohjigyun <epraise12@naver.com>
2022-01-27 23:39:30 +0900

    1/27 algorithm

--- Hash_BestAlbum.js
+++ Hash_BestAlbum.js
@@ -1,4 +1,29 @@
 function solution(genres, plays) {
+    const genreMap = new Map();
+    
+    genres
+        .map((genre, index) => [genre , plays[index]])
+        .forEach(([genre, play] , index)=>{
+            const data = genreMap.get(genre) || { total : 0 , songs : []}
+            genreMap.set(genre, {
+                total : data.total + play,
+                songs : [...data.songs, { play , index }]
+                    .sort((a,b)=> b.play - a.play)
+                    .slice(0,2)
+            })
+    })
+    
+    return [...genreMap.entries()]
+            .sort((a,b) => b[1].total - a[1].total)
+            .flatMap(item => item[1].songs)
+            .map(el => el.index)
+    
+    
+}
+
+/*
+
+    //키 : 장르 , 밸류 : 총 횟수로 객체 만들기
     const obj = {};
     for(let i = 0 ; i < genres.length; i++){
         if(!Object.keys(obj).includes(genres[i])){
@@ -18,6 +43,7 @@ function solution(genres, plays) {
             }
         }
         arr.sort((a,b)=>b-a)
+        //plays 배열에 arr[0]에 해당하는 
         result.push(plays.indexOf(arr[0]))
         plays.splice(plays.indexOf(arr[0]), 1 , 0)
         if(arr[1]){
@@ -26,4 +52,4 @@ function solution(genres, plays) {
         arr=[]
     }
     return result
-}
+*/
